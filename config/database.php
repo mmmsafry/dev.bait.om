965357464aaf34c96eb3c66ce319aefca72cc1af
rails <user>
2018-01-23 16:14:53 +0530

    Database Credentials changes.

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '188.166.152.238'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'admjmdwxtj'),
+            'username' => env('DB_USERNAME', 'admjmdwxtj'),
+            'password' => env('DB_PASSWORD', 'eKV8X7fE8T'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
